f6133301bfc34935c7c7702b49bdd11207c9993e
Kirill Genin <kirill.genin@gmail.com>
2023-04-09 11:50:36 +0300

    + проверка "Таблица 'тираж-цена печати' пустая"

--- processes/algos/AlgAproximate.ts
+++ processes/algos/AlgAproximate.ts
@@ -1,6 +1,6 @@
 // Расчет цены цифровой печати.
 // Есть массив costDigitalPrinting [тираж, себестоимость оттиска] сохраняем в отдельном файле и можем подключать к разным печатным машинам.
-// Находим себест печати cost при заданном тираже detailQuantity
+// Находим себест печати costFound при заданном тираже detailQuantity
 // Находим между какими тиражами leftQuantity и rightQuantity лежит detailQuantity, 
 //   если за пределами отрезка, то берем крайнее значение
 
@@ -24,8 +24,17 @@ export function AlgAproximate(params: AlgInputAprox): AlgOutput {
     arr.sort((a: QuantityCost, b: QuantityCost) => a.runList > b.runList ? 1 : -1);
   }
 
+  let result: AlgOutput = {
+    costFound: 26 // для тестов mock
+  }
+
   let arr = algInput.costDigitalPrintingArray;
-  sortByRunList(arr);
+
+  if(!arr || !arr.length) {
+    return console.log("Таблица 'тираж-цена печати' пустая");
+  }
+
+  sortByRunList(arr); // отсортировали по возрастанию тиражей
 
   let leftQuantity = 0;
   let rightQuantity = 0;
@@ -33,14 +42,14 @@ export function AlgAproximate(params: AlgInputAprox): AlgOutput {
   let rightCost = arr[0].cost;
   let costFound = 0;
 
-  for(let i = 0; i < arr.length; i++) {
+  for(let i = 0; i < arr.length; i++) { // находим между какими точками тиражей из таблицы лежит искомый тираж
 
     if (algInput.detailRunList >= arr[i].runList) {
       leftQuantity = arr[i].runList;
       leftCost = arr[i].cost;
-        if (typeof arr[i+1] === 'undefined') {
+        if (typeof arr[i+1] === 'undefined') { // искомый тираж за пределами макс-го тиража в таблице
           rightQuantity = arr[i].runList;
-          rightCost =  arr[i].cost;
+          rightCost =  arr[i].cost; 
         }else{
           rightQuantity = arr[i+1].runList;
           rightCost =  arr[i+1].cost;
@@ -52,9 +61,9 @@ export function AlgAproximate(params: AlgInputAprox): AlgOutput {
     };
   }
 
-  if (rightQuantity - leftQuantity == 0) {
+  if (rightQuantity - leftQuantity == 0) { // искомый тираж совпадает с тиражом в таблице
     costFound = rightCost;
-  } else {
+  } else { // линейная интерполяция по двум точкам
     costFound = (rightCost - leftCost) * (algInput.detailRunList - leftQuantity) / (rightQuantity - leftQuantity) + leftCost;
   }
 
@@ -62,9 +71,7 @@ export function AlgAproximate(params: AlgInputAprox): AlgOutput {
     "leftCost = ", leftCost, "rightCost = ", rightCost);
   console.log("costFound: ", costFound);
 
-  const result: AlgOutput = {
-    costFound: 26 // для тестов mock
-  }
+
   
   return result;
 }
@@ -72,10 +79,11 @@ export function AlgAproximate(params: AlgInputAprox): AlgOutput {
 let algInput: AlgInputAprox = {
   detailRunList: 250,
   costDigitalPrintingArray: [
-    {"runList": 5, "cost": 36},
-    {"runList": 10, "cost": 32},
-    {"runList": 20, "cost": 30},
-    {"runList": 30, "cost": 20}],
+    // {"runList": 5, "cost": 36},
+    // {"runList": 10, "cost": 32},
+    // {"runList": 20, "cost": 30},
+    // {"runList": 30, "cost": 20}], 
+  ]
 }
 
 let costFound = AlgAproximate(algInput).costFound;
